ff39794906a880c29dff37d9ca72e2be77d366ac
Saul Vieira <vieira_saul@yahoo.com.br>
2023-12-21 09:51:01 -0300

    refactor: separating into more organized functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,8 @@
 const roundsSection = document.getElementById("rounds");
-const slider = document.getElementById("rounds-slider");
 const roundElement = document.getElementById("round-number");
 const errorElement = document.getElementById("error");
 const errorMessage = document.getElementById("error-message");
 const loader = document.getElementById("loader");
-let roundNumber = 1;
 
 function toggleLoader() {
     if (loader.style.display == "flex") {
@@ -14,97 +12,109 @@ function toggleLoader() {
     }
 }
 
-(async function getRounds() {
+function showError(message) {
+    errorElement.style.display = "flex";
+    errorMessage.innerHTML = message;
+    roundsSection.style.display = "none";
+}
+
+const results = [];
+
+async function getRounds() {
     try {
         toggleLoader();
         const response = await fetch("https://sevn-pleno-esportes.deno.dev");
         const rounds = await response.json();
         toggleLoader();
-
-        rounds.map(function (round) {
-            let slideElement = document.createElement("div");
-            slideElement.classList.add("slide");
-            slider.appendChild(slideElement);
-            round.games.map(function (game) {
-                slideElement.insertAdjacentHTML(
-                    "beforeend",
-                    `                    
-                    <div class="game">
-                        <div class="team home">
-                            <img class="logo" src="assets/img/${game.team_home_id}.png" alt="Logo do ${game.team_home_name}">
-                            <div class="name">${game.team_home_name}</div>
-                            <div class="score">${game.team_home_score}</div>
-                        </div>
-                        <img class="versus" src="assets/img/versus.svg" alt="versus X icon">
-                        <div class="team away">
-                            <div class="score">${game.team_away_score}</div>
-                            <div class="name">${game.team_away_name}</div>                            
-                            <img class="logo" src="assets/img/${game.team_away_id}.png" alt="Logo do ${game.team_away_name}">
-                        </div>
-                    </div>
-                `
-                );
-            });
-        });
-
-        const slidesTeste = slider.getElementsByClassName("slide");
-        const slides = Array.from(slidesTeste);
-        const previousButton = document.querySelector(".navigation.prev");
-        const nextButton = document.querySelector(".navigation.next");
-
-        slides.forEach((slide, index) => {
-            slide.style.transform = `translateX(${index * 100}%)`;
-        });
-
-        let currentSlide = 0;
-        let maxSlide = slides.length - 1;
-
-        function moveSlide() {
-            slides.forEach((slide, index) => {
-                slide.style.transform = `translateX(${
-                    100 * (index - currentSlide)
-                }%)`;
-            });
-            roundElement.innerHTML = roundNumber;
-            updateNavigationButtons();
-        }
-
-        previousButton.addEventListener("click", function () {
-            if (currentSlide === 0) {
-                return;
-            }
-            roundNumber--;
-            currentSlide--;
-            moveSlide();
-        });
-
-        nextButton.addEventListener("click", function () {
-            if (currentSlide === maxSlide) {
-                return;
-            }
-            roundNumber++;
-            currentSlide++;
-            moveSlide();
-        });
-
-        function updateNavigationButtons() {
-            previousButton.classList.remove("disabled");
-            nextButton.classList.remove("disabled");
-
-            if (currentSlide === 0) {
-                previousButton.classList.add("disabled");
-            }
-
-            if (currentSlide === maxSlide) {
-                nextButton.classList.add("disabled");
-            }
-        }
-
-        updateNavigationButtons();
+        rounds.forEach((el) => results.push(el));
     } catch (error) {
-        errorElement.style.display = "flex";
-        errorMessage.innerHTML = `${error.name} - ${error.message}`;
-        toggleLoader();
-        roundsSection.style.display = "none";
+        showError(`${error.name} - ${error.message}`);
     }
-})();
+}
+
+window.onload = async () => {
+    await getRounds();
+
+    const slider = document.getElementById("rounds-slider");
+
+    results?.map(function (round) {
+        let slideElement = document.createElement("div");
+        slideElement.classList.add("slide");
+        slider.appendChild(slideElement);
+        round.games.map(function (game) {
+            slideElement.insertAdjacentHTML(
+                "beforeend",
+                `                    
+                <div class="game">
+                <div class="team home">
+                <img class="logo" src="assets/img/${game.team_home_id}.png" alt="Logo do ${game.team_home_name}">
+                <div class="name">${game.team_home_name}</div>
+                <div class="score">${game.team_home_score}</div>
+                </div>
+                <img class="versus" src="assets/img/versus.svg" alt="versus X icon">
+                <div class="team away">
+                <div class="score">${game.team_away_score}</div>
+                <div class="name">${game.team_away_name}</div>                            
+                <img class="logo" src="assets/img/${game.team_away_id}.png" alt="Logo do ${game.team_away_name}">
+                </div>
+                </div>
+                `
+            );
+        });
+    });
+
+    const slides = Array.from(slider.getElementsByClassName("slide"));
+    const previousButton = document.querySelector(".navigation.prev");
+    const nextButton = document.querySelector(".navigation.next");
+
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(${index * 100}%)`;
+    });
+
+    let currentSlide = 0;
+    let maxSlide = slides.length - 1;
+    let roundNumber = 1;
+
+    function moveSlide() {
+        slides.forEach((slide, index) => {
+            slide.style.transform = `translateX(${
+                100 * (index - currentSlide)
+            }%)`;
+        });
+        roundElement.innerHTML = roundNumber;
+        updateNavigationButtons();
+    }
+
+    previousButton.addEventListener("click", function () {
+        if (currentSlide === 0) {
+            return;
+        }
+        roundNumber--;
+        currentSlide--;
+        moveSlide();
+    });
+
+    nextButton.addEventListener("click", function () {
+        if (currentSlide === maxSlide) {
+            return;
+        }
+        roundNumber++;
+        currentSlide++;
+        moveSlide();
+    });
+
+    function updateNavigationButtons() {
+        previousButton.classList.remove("disabled");
+        nextButton.classList.remove("disabled");
+
+        if (currentSlide === 0) {
+            previousButton.classList.add("disabled");
+        }
+
+        if (currentSlide === maxSlide) {
+            nextButton.classList.add("disabled");
+        }
+    }
+
+    updateNavigationButtons();
+};
